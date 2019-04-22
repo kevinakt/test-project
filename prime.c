021a1ca2268e7c17e2fca1b15b33f139a2b69de0
kevinakt <kevin_akt@amata.co.jp>
2019-04-22 10:07:00 +0900

    更新

--- prime.c
+++ prime.c
@@ -3,14 +3,10 @@
 
 int isNumber(char number[])
 {
-    int i = 0;
-
-    //checking for negative numbers
-    if (number[0] == '-')
-        i = 1;
-    for (; number[i] != 0; i++)
+    if (number[0] == '-' || number[0] == '0')
+        return  0;
+    for (int i = 0; number[i] != 0; i++)
     {
-        //if (number[i] > '9' || number[i] < '0')
         if (!isdigit(number[i]))
             return 0;
     }
@@ -20,14 +16,18 @@ int isNumber(char number[])
 int main(int argc, char *argv[])
 {	
 	if(!isNumber(argv[1])){
-		printf("エラー！%sは数字じゃありません。\n", argv[1]);
+		printf("エラー！\n", argv[1]);
 	} else {
 		int numb = atoi(argv[1]);
-	
-		for(int i=1; i < numb; i++){
-			if(i%2 != 0) {
-				printf("%d ", i);
+		int counter;
+		for(int i=1; i <= numb; i++){
+			counter = 0;
+			for(int j = 1; j < i; j++){
+				if(i % j == 0) {
+					counter++;
+				}
 			}
+			if(counter < 2) printf("%d ", i);
 		}
 
 	}
