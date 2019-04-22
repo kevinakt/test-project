c8fd84991c475f2b4a9718c44a8ee8e0bb2a46e9
kevinakt <kevin_akt@amata.co.jp>
2019-04-22 14:58:51 +0900

    Unity project 追加

---
+++ unity_test/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChangeScene : MonoBehaviour {
+
+    public GameObject tileButton, mainButton, endButton;
+
+    private void Awake()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+        tileButton.active = true;
+        mainButton.active = true;
+        endButton.active = true;
+        switch (sceneName)
+        {
+            case "Tile":
+                tileButton.active = false;
+                break;
+            case "Main":
+                mainButton.active = false;
+                break;
+            case "End":
+                endButton.active = false;
+                break;
+        }
+    }
+
+    public void GoToTileScene()
+    {
+        Debug.Log("Tile Button is clicked.");
+        SceneManager.LoadScene("Tile");
+    }
+
+    public void GoToMainScene()
+    {
+        Debug.Log("Main Button is clicked.");
+        SceneManager.LoadScene("Main");
+    }
+
+    public void GoToEndScene()
+    {
+        Debug.Log("End Button is clicked.");
+        SceneManager.LoadScene("End");
+    }
+}
---
+++ unity_test/Assets/Scripts/DisplayVersion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayVersion : MonoBehaviour {
+
+    public Text displayText;
+
+	void Start () {
+        displayText.text = "Version : " + Application.version;
+	}
+
+}
