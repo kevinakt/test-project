f6d4f741de2ed0e7b5c5a10d2d7427aa8fe4bf41
kevinakt <kevin_akt@amata.co.jp>
2019-05-24 15:51:26 +0900

    First fibonacci

---
+++ unity_test/Assets/Editor/FibonacciTest.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FibonacciTest {
+
+    CalculateFibonacci calFibo = new CalculateFibonacci();
+
+    [Test]
+    public void CheckResult() {
+        int result = calFibo.fibonacci(10);
+        
+        Assert.AreEqual(55, result);
+    }
+
+    [Test]
+    public void InputMinus(){
+        bool result = calFibo.Calculate(-10);
+        
+        Assert.IsFalse(result);
+    }
+
+    [Test]
+    public void InputZero(){
+        bool result = calFibo.Calculate(0);
+        
+        Assert.IsFalse(result);
+    }
+
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    // [UnityTest]
+    // public IEnumerator FibonacciTestWithEnumeratorPasses() {
+    //     // Use the Assert class to test conditions.
+    //     // yield to skip a frame
+    //     yield return null;
+    // }
+}
---
+++ unity_test/Assets/Scripts/CalculateFibonacci.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalculateFibonacci : MonoBehaviour {
+
+	[SerializeField] InputField inputBox;
+	[SerializeField] Text resultText;
+	string result = "";
+
+	public bool Calculate(int input = 0) {
+		InputField inputScript = inputBox.GetComponent<InputField>();
+		result = "";
+		input = int.Parse(inputScript.text);
+
+		if(input >= 1) {
+
+			fibonacci(input);
+
+			for(int i = 1; i <= input; i++) {
+				result += fibonacci(i) + " ";
+			}
+			
+			resultText.text ="Result : " + result;
+
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	public int fibonacci(int n) {
+		if(n == 0) {
+			return 0;
+		} else if(n == 1) {
+			return 1;
+		} else {
+			return fibonacci(n-1) + fibonacci(n-2);
+		}
+	}
+}
